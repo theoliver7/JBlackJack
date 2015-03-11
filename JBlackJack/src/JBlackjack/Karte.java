f5bbe1c5677548b04c30f78a9c66fc0703953a34
Oliver Aschwanden, ICT Berufsbildungscenter AG, oliver.aschwanden@bbcag.ch <Oliver Aschwanden, ICT Berufsbildungscenter AG, oliver.aschwanden@bbcag.ch@WS-ZASCHO.bbcnet-zh.local>
2015-03-11 17:19:14 +0100

    willi glust gha han

--- JBlackJack/src/JBlackjack/Karte.java
+++ JBlackJack/src/JBlackjack/Karte.java
@@ -2,4 +2,31 @@ package JBlackjack;
 
 public class Karte {
 
+	private String sorte;
+	private int wert;
+
+	
+	
+	
+	
+	
+	
+	
+	
+	public String getSorte() {
+		return sorte;
+	}
+
+	public void setSorte(String sorte) {
+		this.sorte = sorte;
+	}
+
+	public int getWert() {
+		return wert;
+	}
+
+	public void setWert(int wert) {
+		this.wert = wert;
+	}
+	
 }
--- JBlackJack/src/JBlackjack/Person.java
+++ JBlackJack/src/JBlackjack/Person.java
@@ -4,6 +4,23 @@ import java.awt.List;
 
 public abstract class Person {
 	Spiel Spiel_1 = new Spiel();
-	List AktuelleKarten = new List();
 	
+	List AktuelleKarten = new List();
+
+	public void kartenehmen() {
+
+	}
+
+	public void verwerfen() {
+
+	}
+
+	public void assueberpruefung() {
+
+	}
+
+	public void wertueberpruefung() {
+
+	}
+
 }
