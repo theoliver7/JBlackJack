6d932bfe88710226466f69ec0108af667f770269
zjorgm <zjorgm@ws-zjorgm.bbcnet-zh.local>
2015-03-11 07:54:40 +0100

    commit?

--- JBlackJack/src/JBlackjack/Blackjack.java
+++ JBlackJack/src/JBlackjack/Blackjack.java
@@ -1,24 +1,28 @@
 package JBlackjack;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Container;
+import java.awt.Dimension;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class Blackjack extends JFrame {
 
+	private static JButton Kartenehmen = new JButton("Karte Ziehen");
+
 	public static void main(String[] args) {
 		Blackjack Spiel = new Blackjack();
-		Spiel.getContentPane().setBackground(new Color(8,138,8)); 
+		Spiel.setSize(800, 700);
+		Spiel.setVisible(true);
+		Spiel.add(Kartenehmen, BorderLayout.SOUTH);
+		Kartenehmen.setPreferredSize(new Dimension(100,100));
+		Kartenehmen.setVisible(true);
 	}
 
 	public Blackjack() {
 		setTitle("JBlackJack");
-		setBackground(Color.green);
-		setSize(800, 700);
-		setVisible(true);
-		setDefaultCloseOperation(EXIT_ON_CLOSE); 
-		
-
+		getContentPane().setBackground(new Color(8, 138, 8));		
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 }
